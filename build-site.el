3497d66441753c3a0f518d36ad6a30723722e2fe
Michael Constans <michael.constans@enel.com>
2023-04-26 17:06:34 -0700

    Added some comments to some build steps

--- build-site.el
+++ build-site.el
@@ -1,5 +1,4 @@
-;; Set the packaage installation directory so that packages aren't stored in the
-;; ~/.emacs.d/elpa path
+;; Set the packaage installation directory so that packages aren't stored in the ~/.emacs.d/elpa path
 (require 'package)
 (setq package-user-dir (expand-file-name "./packages"))
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -16,9 +15,10 @@
 ;; load the publishing system
 (require 'ox-publish)
 
-(setq org-html-validation-link nil
-      org-html-head-include-scripts nil
-      org-html-head-include-default-style nil
+;; Customize the html output
+(setq org-html-validation-link nil             ;; don't show the validation link
+      org-html-head-include-scripts nil        ;; use our own scripts
+      org-html-head-include-default-style nil  ;; use our own styles
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
 ;; Define the publishing project
@@ -27,13 +27,13 @@
        (list "org-site:main"
              :recursive t
              :base-directory "./content"
-             :publishing-directory "./public"
              :publishing-function 'org-html-publish-to-html
-             :with-author nil
-             :with-creator t
-             :with-toc t
-             :section-numbers nil
-             :time-stamp-file nil)))
+             :publishing-directory "./public"
+             :with-author nil                  ;; don't include author name
+             :with-creator t                   ;; include emacs and org versions in footer
+             :with-toc t                       ;; include a table of contents
+             :section-numbers nil              ;; don't include section numbers
+             :time-stamp-file nil)))           ;; don't include time stamp in file
 
 ;; Generate the site output
 (org-publish-all t)
